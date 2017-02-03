e313b04426a3edbfcada588055ad8f2dca75fae7
chavab1 <chavab_1@yahoo.com>
2017-02-03 15:39:05 -0800

    Change image directory from images/ to media/

--- script.php
+++ script.php
@@ -4,8 +4,39 @@
 
     class com_folioInstallerScript
     {
+        function createSampleImages()
+        {
+            //Imports all standard Joomla classes
+            jimport( 'joomla.filesystem.file' );
+
+            //Check if directory exists
+            if(!file_exists(JPATH_SITE . "/images/com_folio/"))
+            {
+                JFolder::create(JPATH_SITE. "/images/com_folio/");
+            }
+
+            if(!file_exists(JPATH_SITE. "/images/com_folio/index.html"))
+            {
+                JFile::copy(JPATH_SITE."/media/com_folio/index.html",JPATH_SITE. "/images/com_folio/index.html");
+            }
+            //Check if images exist, otherwise copy from media directory to images directory
+            if(!file_exists(JPATH_SITE. "/images/com_folio/chocolate.png"))
+            {
+                JFile::copy(JPATH_SITE."/media/com_folio/images/chocolate. png",JPATH_SITE. "/images/com_folio/chocolate.png");
+            }
+            if(!file_exists(JPATH_SITE. "/images/com_folio/coin.png"))
+            {
+                JFile::copy(JPATH_SITE."/media/com_folio/images/coin. png",JPATH_SITE. "/images/com_folio/coin.png");
+            }
+            if(!file_exists(JPATH_SITE. "/images/com_folio/cookie.png"))
+            {
+                JFile::copy(JPATH_SITE."/media/com_folio/images/cookie. png",JPATH_SITE. "/images/com_folio/cookie.png");
+            }
+        }
+
         function install($parent)
         {
+            $this->createSampleImages();
             $parent->getParent()->setRedirectURL('index.php?option=com_folio');
         }
 
@@ -16,6 +47,7 @@
 
         function update($parent)
         {
+            $this->createSampleImages();
             echo '<p>' . JText::_('COM_FOLIO_UPDATE_TEXT') . '</p>';
         }
 
